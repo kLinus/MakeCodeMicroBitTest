a948f7b25178cdf3a7b38dea04d3a6adcd7053e6
Linus <atomskv1@gmail.com>
2020-09-20 13:58:20 +0530

    Added a happy face on Button A and a sad face on Button B

--- main.ts
+++ main.ts
@@ -1,1 +1,18 @@
-
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . # . # .
+        . # . # .
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . # . # .
+        . # . # .
+        . . . . .
+        . # # # .
+        # . . . #
+        `)
+})
